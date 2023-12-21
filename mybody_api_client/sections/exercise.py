a38e9d43b7b913be4257dadd5fadd5faa78e4044
artemkovsh <135865999+artemkovshgit@users.noreply.github.com>
2023-12-21 14:24:21 +0300

    Added Training and Exercise section

---
+++ mybody_api_client/sections/exercise.py
@@ -0,0 +1,69 @@
+#
+# (c) 2023, Yegor Yakubovich, yegoryakubovich.com, personal@yegoryakybovich.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+from mybody_api_client.sections.base import BaseSection, RequestTypes
+
+
+class Exercise(BaseSection):
+    prefix = 'exercises'
+
+    async def get_list(self):
+        path = '/list/get'
+        response = await self.request(
+            type_=RequestTypes.GET,
+            path=path,
+        )
+
+        return response
+
+    async def create(self, name: str, type_: str):
+        path = '/create'
+        response = await self.request(
+            type_=RequestTypes.POST,
+            path=path,
+            parameters={
+                'name': name,
+                'type': type_
+            },
+        )
+
+        return response
+
+    async def update(self, id_: int, type_: str):
+        path = '/list/get'
+        response = await self.request(
+            type_=RequestTypes.POST,
+            path=path,
+            parameters={
+                'id': id_,
+                'type': type_,
+            },
+        )
+
+        return response
+
+    async def delete(self, id_: int):
+        path = '/delete'
+        response = await self.request(
+            type_=RequestTypes.POST,
+            path=path,
+            parameters={
+                'id': id_,
+            },
+        )
+
+        return response
